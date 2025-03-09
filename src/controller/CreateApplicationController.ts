401addf88cbd98fc3e2dcd461df95af30a7d3973
Vinicius Lima <102876820+v-L1ma@users.noreply.github.com>
2025-03-09 18:02:56 -0300

    feat: adding a validation to verify if already exists

--- src/controller/CreateApplicationController.ts
+++ src/controller/CreateApplicationController.ts
@@ -17,32 +17,50 @@ export class CreateApplicationController {
       situacao,
       escolaridade,
       curso,
-      instituição,
+      instituicao,
       dataInicioEstudo,
       dataTerminoEstudos,
     } = req.body;
 
-    const application = await prisma.application.create({
-      data: {
-      nomeCompleto,
-      email,
-      telefone,
-      dataNasc,
-      cpf,
-      cargo,
-      empresa,
-      dataInicioEmpresa,
-      dataTerminoEmpresa,
-      descricaoATVD,
-      situacao,
-      escolaridade,
-      curso,
-      instituição,
-      dataInicioEstudo,
-      dataTerminoEstudos,
-      },
-    });
+    try {
+      
+      const isAlreadyApplied = await prisma.application.findFirst({
+        where: {
+          OR:[{email}, {cpf}],
+        }
+      })
 
-    return res.status(200).json({application:application});
+      if(isAlreadyApplied){
+        return res.status(400).json({msg:"Already applied to this job vacancy"});
+      }
+  
+      const application = await prisma.application.create({
+        data: {
+        nomeCompleto,
+        email,
+        telefone,
+        dataNasc,
+        cpf,
+        cargo,
+        empresa,
+        dataInicioEmpresa,
+        dataTerminoEmpresa,
+        descricaoATVD,
+        situacao,
+        escolaridade,
+        curso,
+        instituicao,
+        dataInicioEstudo,
+        dataTerminoEstudos,
+        },
+      });
+  
+      return res.status(200).json({application:application});
+
+    } catch (error) {
+      console.log(error)
+      return res.status(500).json({error: error})
+    }
+
   }
 }

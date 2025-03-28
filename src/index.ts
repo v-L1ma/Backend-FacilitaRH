fc3fab2b651a0b85ea40f9a0296186e0e1ffedfa
Vinicius Lima <102876820+v-L1ma@users.noreply.github.com>
2025-03-28 15:02:32 -0300

    Fix: fixing cors error

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,9 @@ import cors from "cors";
 
 const app = express();
 
-app.use(cors())
+app.use(cors({
+  origin: 'https://facilita-rh.netlify.app', 
+}));
 app.use(express.json());
 app.use(router);
 
